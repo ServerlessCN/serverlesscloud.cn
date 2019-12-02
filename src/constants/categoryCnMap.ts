134382446bc43baea5f53b6e7348bfffe4b8fc76
juliiiizhu <juliiiizhu@tencent.com>
2019-12-02 11:39:49 +0800

    fix: spell error

--- src/constants/categoryCnMap.ts
+++ src/constants/categoryCnMap.ts
@@ -6,6 +6,6 @@ export const categoryCnMap = {
   'engineering-culture': '工程师文化',
   'guides-and-tutorials': '指南和教程',
   news: '新闻',
-  'operation-and-observatity': '运维与监控',
+  'operations-and-observability': '运维与监控',
   'user-stories': '用户故事',
 }
