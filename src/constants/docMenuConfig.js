384c252549d273cac2c609cebc24c23523919a9b
tinafang(方坤丁) <tinafang@tencent.com>
2019-12-05 00:08:01 +0800

    update

--- src/constants/docMenuConfig.js
+++ src/constants/docMenuConfig.js
@@ -2,101 +2,117 @@ const baseUrl = '/doc'
 
 let config = [
   {
-    label: '快速入门',
-    to: `${baseUrl}/quick/serverless-framework`,
+    label: '概述',
+    to: `${baseUrl}/overview`,
+    label: '快速开始',
+    to: `${baseUrl}/quick-start`,
+    label: '支持者 CLI 文档',
+    content: [
+      {
+        content: [
+          {
+            label: '快速开始',
+            to: `${baseUrl}/providers/tencent/cli-reference/quick-start`,
+          },
+          {
+            label: '配置账号',
+            to: `${baseUrl}/providers/tencent/cli-reference/configure`,
+          },
+          {
+            label: '创建服务',
+            to: `${baseUrl}/providers/tencent/cli-reference/create`,
+          },
+          {
+            label: '打包服务',
+            to: `${baseUrl}/providers/tencent/cli-reference/package`,
+          },
+          {
+            label: '部署服务',
+            to: `${baseUrl}/providers/tencent/cli-reference/deploy`,
+          },
+          {
+            label: '部署函数',
+            to: `${baseUrl}/providers/tencent/cli-reference/deploy-function`,
+          },
+          {
+            label: '部署列表',
+            to: `${baseUrl}/providers/tencent/cli-reference/delpoy-list`,
+          },
+          {
+            label: '云端调用',
+            to: `${baseUrl}/providers/tencent/cli-reference/invoke`,
+          },
+          {
+            label: '日志查看',
+            to: `${baseUrl}/providers/tencent/cli-reference/logs`,
+          },
+          {
+            label: '回滚服务',
+            to: `${baseUrl}/providers/tencent/cli-reference/rollback`,
+          },
+          {
+            label: '删除服务',
+            to: `${baseUrl}/providers/tencent/cli-reference/delete`,
+          },
+          {
+            label: '获取详情',
+            to: `${baseUrl}/providers/tencent/cli-reference/info`,
+          },
+          {
+            label: '运行数据统计',
+            to: `${baseUrl}/providers/tencent/cli-reference/metrics`,
+          },
+        ],
+        content: [
+          {
+            label: 'apigateway',
+            to: `${baseUrl}/providers/tencent/events/apigateway`,
+          },
+          {
+            label: 'cos',
+            to: `${baseUrl}/providers/tencent/events/cos`,
+          },
+          {
+            label: 'cloud kafka',
+            to: `${baseUrl}/providers/tencent/events/cloud-kafka`,
+          },
+          {
+            label: 'cmq',
+            to: `${baseUrl}/providers/tencent/events/cmq`,
+          },
+          {
+            label: 'timer',
+            to: `${baseUrl}/providers/tencent/events/timer`,
+          },
+        ]
+      },
+      {
+        label: '支持者 Component 文档',
+        content: [
+          {
+            label: '云函数SCF组件',
+            to: `${baseUrl}/操作指南/Components/云函数SCF组件`,
+          },
+          {
+            label: '对象存储 COS 组件',
+            to: `${baseUrl}/操作指南/Components/对象存储 COS 组件`,
+          },
+          {
+            label: 'API 网关组件',
+            to: `${baseUrl}/操作指南/Components/API 网关组件`,
+          },
+          {
+            label: '访问管理 CAM-策略组件',
+            to: `${baseUrl}/操作指南/Components/访问管理 CAM-策略组件`,
+          },
+          {
+            label: '访问管理 CAM-角色组件',
+            to: `${baseUrl}/操作指南/Components/访问管理 CAM-角色组件`,
+          },
+        ],
+      },
+    ],
   },
-  // {
-  //   label: '操作指南',
-  //   content: [
-  //     {
-  //       label: 'Framework CLI',
-  //       content: [
-  //         {
-  //           label: '快速安装',
-  //           to: `${baseUrl}/操作指南/Framework CLI/快速安装`,
-  //         },
-  //         {
-  //           label: '配置账号',
-  //           to: `${baseUrl}/操作指南/Framework CLI/配置账号`,
-  //         },
-  //         {
-  //           label: '创建服务',
-  //           to: `${baseUrl}/操作指南/Framework CLI/创建服务`,
-  //         },
-  //         {
-  //           label: '打包服务',
-  //           to: `${baseUrl}/操作指南/Framework CLI/打包服务`,
-  //         },
-  //         {
-  //           label: '部署服务',
-  //           to: `${baseUrl}/操作指南/Framework CLI/部署服务`,
-  //         },
-  //         {
-  //           label: '部署函数',
-  //           to: `${baseUrl}/操作指南/Framework CLI/部署函数`,
-  //         },
-
-  //         {
-  //           label: '云端调用',
-  //           to: `${baseUrl}/操作指南/Framework CLI/云端调用`,
-  //         },
-  //         {
-  //           label: '日志查看',
-  //           to: `${baseUrl}/操作指南/Framework CLI/日志查看`,
-  //         },
-  //         {
-  //           label: '回滚服务',
-  //           to: `${baseUrl}/操作指南/Framework CLI/回滚服务`,
-  //         },
-  //         {
-  //           label: '删除服务',
-  //           to: `${baseUrl}/操作指南/Framework CLI/删除服务`,
-  //         },
-  //         {
-  //           label: '部署列表',
-  //           to: `${baseUrl}/操作指南/Framework CLI/部署列表`,
-  //         },
-  //         {
-  //           label: '获取详情',
-  //           to: `${baseUrl}/操作指南/Framework CLI/获取详情`,
-  //         },
-  //         {
-  //           label: '运行数据统计',
-  //           to: `${baseUrl}/操作指南/Framework CLI/运行数据统计`,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       label: 'Components',
-  //       content: [
-  //         {
-  //           label: 'Components 概述',
-  //           to: `${baseUrl}/操作指南/Components/Components 概述`,
-  //         },
-  //         {
-  //           label: '云函数SCF组件',
-  //           to: `${baseUrl}/操作指南/Components/云函数SCF组件`,
-  //         },
-  //         {
-  //           label: '对象存储 COS 组件',
-  //           to: `${baseUrl}/操作指南/Components/对象存储 COS 组件`,
-  //         },
-  //         {
-  //           label: 'API 网关组件',
-  //           to: `${baseUrl}/操作指南/Components/API 网关组件`,
-  //         },
-  //         {
-  //           label: '访问管理 CAM-策略组件',
-  //           to: `${baseUrl}/操作指南/Components/访问管理 CAM-策略组件`,
-  //         },
-  //         {
-  //           label: '访问管理 CAM-角色组件',
-  //           to: `${baseUrl}/操作指南/Components/访问管理 CAM-角色组件`,
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
   {
     label: '最佳实践',
     to: `${baseUrl}/best-practice/express`,
