0f439b10f30fd4804e1aeae64d9159692b2471d3
Juliiii <zynleifung@gmail.com>
2019-12-11 17:37:09 +0800

    fix: remove offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,8 +75,10 @@ module.exports = {
         icon: `src/assets/images/icon-serverless-framework.png`, // This path is relative to the root of the site.
       },
     },
+    // this plugin is awaful
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
   ],
 }
