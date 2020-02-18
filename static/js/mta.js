b8de82796618fb497e67ada2bb571d0adfa9800e
tongtingting <754085143@qq.com>
2020-02-18 21:21:56 +0800

    deploy-test

--- static/js/mta.js
+++ static/js/mta.js
@@ -8,11 +8,3 @@ var _mtac = { senseQuery: 1 };
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(mta, s)
 })();
-(function() {
-    var mta_other = document.createElement("script");
-    mta_other.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
-    mta_other.setAttribute("name", "MTAH5");
-    mta_other.setAttribute("sid", "500710929");
-    var s_other = document.getElementsByTagName("script")[1];
-    s_other.parentNode.insertBefore(mta_other, s_other);
-})();
