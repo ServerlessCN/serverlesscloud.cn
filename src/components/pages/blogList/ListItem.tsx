04ecfe64871ae9aad9db8efd112ceb1fbed276c5
syz <eagle-china@hotmail.com>
2020-03-04 15:10:43 +0800

    change best practice page

--- src/components/pages/blogList/ListItem.tsx
+++ src/components/pages/blogList/ListItem.tsx
@@ -48,36 +48,21 @@ export default function({ data }: { data: Blog }) {
           justifyContent={['center', 'center', 'center', 'flex-start']}
           flexDirection={['column', 'column', 'column', 'row']}
         >
-          <Background
-            width={[0.9, 0.9, 0.9, 0.35]}
-            height={[200]}
-            background={`url(${JSON.stringify(frontmatter.thumbnail)})`}
-            backgroundSize="cover"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-          />
+          
           <BoxWithFlex width={[0.9, 0.9, 0.9, 0.65]} ml={[0, 0, 0, '30px']}>
             <ColumnWithHeight
               alignItems="flex-end"
               justifyContent="space-between"
             >
               <Box width={[1]}>
-                <Text
-                  mt="20px"
-                  mb="10px"
-                  fontSize={['20px', '20px', '20px', '24px', '24px']}
-                  fontWeight="bold"
-                >
-                  {frontmatter.title}
-                </Text>
                 <Box>
                   <InlineText
                     color={theme.colors.gray[2]}
                     fontSize={['12px', '14px']}
                   >
-                    作者: {frontmatter.authors.join(',')}
-                    &nbsp;&nbsp; 发布于{formateDate(frontmatter.date)}
-                    &nbsp;&nbsp;阅读大概需要{timeToRead}分钟
+                    {frontmatter.authors.join(',')}
+                    &nbsp;·&nbsp;{formateDate(frontmatter.date)}
+                    &nbsp;·&nbsp;阅读大概需要{timeToRead}分钟
                     {frontmatter.categories && frontmatter.categories.length
                       ? `  归档于${frontmatter.categories.map(o =>
                           generateCategoryText(o)
@@ -86,6 +71,16 @@ export default function({ data }: { data: Blog }) {
                   </InlineText>
                 </Box>
 
+                <Text
+                  mt="20px"
+                  mb="10px"
+                  fontSize={['20px', '20px', '20px', '24px', '24px']}
+                  fontWeight="bold"
+                >
+                  {frontmatter.title}
+                </Text>
+                
+
                 <Row mt="10px">
                   <Text lineHeight={1.75} fontSize={'16px'} mt="20px" mb="20px">
                     {frontmatter.description}
@@ -93,11 +88,19 @@ export default function({ data }: { data: Blog }) {
                 </Row>
               </Box>
 
-              <Button width="160px" p="0.4rem" fontSize="18px" theme={theme}>
-                阅读全文
-              </Button>
+              {/*<Button width="160px" p="0.4rem" fontSize="18px" theme={theme}>
+                              阅读全文
+                            </Button>*/}
             </ColumnWithHeight>
           </BoxWithFlex>
+          <Background
+            width={[0.9, 0.9, 0.9, 0.35]}
+            height={[200]}
+            background={`url(${JSON.stringify(frontmatter.thumbnail)})`}
+            backgroundSize="cover"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+          />
         </Flex>
       </Box>
     </BlogDetailLink>
