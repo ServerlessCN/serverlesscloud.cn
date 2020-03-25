a88c2b7e719e7a6b1b3aa95b5560d562d7fe3ebc
syz <eagle-china@hotmail.com>
2020-03-25 15:14:03 +0800

    change about communitys

---
+++ src/constants/communitys.ts
@@ -0,0 +1,47 @@
+/**
+ * 关于页面合作社区配置
+ * name: 社区名称
+ * logo: 社区图片
+ */
+import ImgCloudplus from '@src/assets/images/cloudplus.png'
+import ImgCoding from '@src/assets/images/coding.png'
+import ImgDevops from '@src/assets/images/devops.png'
+import ImgDbaplus from '@src/assets/images/dbaplus.png'
+import ImgInfoQ from '@src/assets/images/infoQ.png'
+import ImgWuhan2020 from '@src/assets/images/wuhan2020.png'
+
+const communitys = [
+    {
+        name: '云加社区',
+        logo: ImgCloudplus,
+        link: 'javascript:;'
+    },
+    {
+        name: 'coding',
+        logo: ImgCoding,
+        link: 'javascript:;'
+    },
+    {
+        name: 'infoQ',
+        logo: ImgInfoQ,
+        link: 'javascript:;'
+    },
+    {
+        name: '中国devops社区',
+        logo: ImgDevops,
+        link: 'javascript:;'
+    },
+    {
+        name: 'dbaplus',
+        logo: ImgDbaplus,
+        link: 'javascript:;'
+    },
+    
+    {
+        name: 'wuhan2020',
+        logo: ImgWuhan2020,
+        link: 'javascript:;'
+    }
+]
+
+export default communitys;
