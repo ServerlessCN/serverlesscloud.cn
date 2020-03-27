26396c92074152d5b9c5df01918ad7cb094ccf06
aceyclee <1159583291@qq.com>
2020-03-27 09:33:17 +0800

    1. update avatar 2. update new meetup

--- src/constants/contributors.ts
+++ src/constants/contributors.ts
@@ -8,6 +8,7 @@ const contributors = [
   {
     name: 'Austen',
     link: 'https://github.com/ac360',
+    avatar: 'https://avatars.githubusercontent.com/ac360',
   },
   {
     name: 'Aceyclee',
@@ -20,54 +21,67 @@ const contributors = [
   {
     name: 'Avery Harnish',
     link: 'https://github.com/EverlastingBugstopper',
+    avatar: 'https://avatars.githubusercontent.com/EverlastingBugstopper',
   },
   {
     name: 'Benny Bauer',
     link: 'https://github.com/bennybauer',
+    avatar: 'https://avatars.githubusercontent.com/bennybauer',
   },
   {
     name: 'Bill Fine',
     link: 'https://github.com/billfine',
+    avatar: 'https://avatars.githubusercontent.com/billfine',
   },
   {
     name: 'Çağatay Gürtürk',
     link: 'https://github.com/cagataygurturk',
+    avatar: 'https://avatars.githubusercontent.com/cagataygurturk',
   },
   {
     name: 'CanMeng',
     link: 'https://github.com/canmengfly',
+    avatar: 'https://avatars.githubusercontent.com/canmengfly',
   },
   {
     name: 'Carlos Santana',
     link: 'https://github.com/csantanapr',
+    avatar: 'https://avatars.githubusercontent.com/csantanapr',
   },
   {
     name: 'Chris Seto',
     link: 'https://github.com/chrisseto',
+    avatar: 'https://avatars.githubusercontent.com/chrisseto',
   },
   {
     name: 'Edouard',
     link: 'https://github.com/sedouard',
+    avatar: 'https://avatars.githubusercontent.com/sedouard',
   },
   {
     name: 'Eetu Tuomala',
     link: 'https://github.com/laardee',
+    avatar: 'https://avatars.githubusercontent.com/laardee',
   },
   {
     name: 'Eslam λ Hefnawy',
     link: 'https://github.com/eahefnawy',
+    avatar: 'https://avatars.githubusercontent.com/eahefnawy',
   },
   {
     name: 'Erik Erikson',
     link: 'https://github.com/erikerikson',
+    avatar: 'https://avatars.githubusercontent.com/erikerikson',
   },
   {
     name: 'Frank Schmid',
     link: 'https://github.com/HyperBrain',
+    avatar: 'https://avatars.githubusercontent.com/HyperBrain',
   },
   {
     name: 'Harry Solovay',
     link: 'https://github.com/harrysolovay',
+    avatar: 'https://avatars.githubusercontent.com/harrysolovay',
   },
   {
     name: 'Iguojincom',
@@ -76,10 +90,12 @@ const contributors = [
   {
     name: 'Jacob Evans',
     link: 'https://github.com/dekz',
+    avatar: 'https://avatars.githubusercontent.com/dekz',
   },
   {
     name: 'James Hall',
     link: 'https://github.com/MrRio',
+    avatar: 'https://avatars.githubusercontent.com/MrRio',
   },
   {
     name: 'Jenswang',
@@ -88,26 +104,32 @@ const contributors = [
   {
     name: 'Jiangliu',
     link: 'https://github.com/jiangliu5267',
+    avatar: 'https://avatars.githubusercontent.com/jiangliu5267',
   },  
   {
     name: 'John McKim',
     link: 'https://github.com/johncmckim',
+    avatar: 'https://avatars.githubusercontent.com/johncmckim',
   },
   {
     name: 'Joost Farla',
     link: 'https://github.com/joostfarla',
+    avatar: 'https://avatars.githubusercontent.com/joostfarla',
   },
   {
     name: 'Jordan Mack',
     link: 'https://github.com/jordanmack',
+    avatar: 'https://avatars.githubusercontent.com/jordanmack',
   },
   {
     name: 'Jorge Chen',
     link: 'https://github.com/chenyingkof',
+    avatar: 'https://avatars.githubusercontent.com/chenyingkof',
   },
   {
     name: 'Josh Wulf',
     link: 'https://github.com/jwulf',
+    avatar: 'https://avatars.githubusercontent.com/jwulf',
   },
   {
     name: 'Juli',
@@ -117,18 +139,22 @@ const contributors = [
   {
     name: 'Kamil Burzynski',
     link: 'https://github.com/Nopik',
+    avatar: 'https://avatars.githubusercontent.com/Nopik',
   },
   {
     name: 'Mark Lawlor',
     link: 'https://github.com/marklawlor',
+    avatar: 'https://avatars.githubusercontent.com/marklawlor',
   },
   {
     name: 'Mariusz Nowak',
     link: 'https://github.com/medikoo',
+    avatar: 'https://avatars.githubusercontent.com/medikoo',
   },
   {
     name: 'Matt Hernandez',
     link: 'https://github.com/fiveisprime',
+    avatar: 'https://avatars.githubusercontent.com/fiveisprime',
   },
   {
     name: 'Nicka',
@@ -137,18 +163,22 @@ const contributors = [
   {
     name: 'Raj Nigam',
     link: 'https://github.com/rajington',
+    avatar: 'https://avatars.githubusercontent.com/rajington',
   },
   {
     name: 'Rémi',
     link: 'https://github.com/remicastaing',
+    avatar: 'https://avatars.githubusercontent.com/remicastaing',
   },
   {
     name: 'Rowan',
     link: 'https://github.com/rowanu',
+    avatar: 'https://avatars.githubusercontent.com/rowanu',
   },
   {
     name: 'Ryan Lewis',
     link: 'https://github.com/ryanmurakami',
+    avatar: 'https://avatars.githubusercontent.com/ryanmurakami',
   },
   {
     name: 'Skierkowski',
@@ -157,38 +187,47 @@ const contributors = [
   {
     name: 'Syz',
     link: 'https://github.com/syzh',
+    avatar: 'https://avatars.githubusercontent.com/syzh',
   },
   {
     name: 'Takahiro Horike',
     link: 'https://github.com/horike37',
+    avatar: 'https://avatars.githubusercontent.com/horike37',
   },
   {
     name: 'Teclipse',
     link: 'https://github.com/tongtingting',
+    avatar: 'https://avatars.githubusercontent.com/tongtingting',
   },
   {
     name: 'Tinafang',
     link: 'https://github.com/tinafangkunding',
+    avatar: 'https://avatars.githubusercontent.com/tinafangkunding',
   },
   {
     name: 'Tyler Neely',
     link: 'https://github.com/spacejam',
+    avatar: 'https://avatars.githubusercontent.com/spacejam',
   },
   {
     name: 'Tom Milewski',
     link: 'https://github.com/tmilewski',
+    avatar: 'https://avatars.githubusercontent.com/tmilewski',
   },
   {
     name: 'Vaidas Mykolaitis',
     link: 'https://github.com/codepreneur',
+    avatar: 'https://avatars.githubusercontent.com/codepreneur',
   },
   {
     name: 'Wallace Breza',
     link: 'https://github.com/wbreza',
+    avatar: 'https://avatars.githubusercontent.com/wbreza',
   },
   {
     name: 'Yermakou',
     link: 'https://github.com/rma4ok',
+    avatar: 'https://avatars.githubusercontent.com/rma4ok',
   },
   {
     name: 'Yuga Sun',
@@ -198,6 +237,7 @@ const contributors = [
   {
     name: 'Zev Mo Green',
     link: 'https://github.com/zevmo',
+    avatar: 'https://avatars.githubusercontent.com/zevmo',
   },
 ]
 
