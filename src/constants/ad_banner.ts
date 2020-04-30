34011ae2b24083f831361c57a9f06db1905100b1
Teclipse <31556215+tongtingting@users.noreply.github.com>
2020-04-30 16:10:02 +0800

    Activity ad banner (#116)
    * add activity-ad-banner
    
    * change ad banner config

---
+++ src/constants/ad_banner.ts
@@ -0,0 +1,11 @@
+ /* 
+    博客详情页活动广告位配置项 
+    adBanner: {
+            link: 活动页地址,
+            thumbnail: 缩略图地址
+        }
+*/
+export const adBanner:{link:string,thumbnail:string} ={
+    link:'https://cloud.tencent.com/apply/p/v1kekxicqhj',
+    thumbnail: 'https://img.serverlesscloud.cn/2020429/1588164013282-banner_hours.jpg'
+}
